7111d2f6a96b2861c1cf614a61a58af6fb4781c0
Gabriel Conti Araujo <103386212+Contiaraujo@users.noreply.github.com>
2026-08-20 17:46:09 -0300

    Fix variable name from jarim_area to jardim_area

--- main2.cpp
+++ main2.cpp
@@ -5,7 +5,7 @@ int main(){
 float comprimento;
 float largura;
 float perimetro;
-float jarim_area;
+float jardim_area;
 
 printf("Digite o comprimento do jardim: ");
 scanf("%f", &comprimento);
@@ -13,12 +13,12 @@ printf("Digite a largura do jardim: ");
 scanf("%f", &largura);
 
 perimetro = 2 * (comprimento + largura);
-jarim_area = comprimento * largura;
+jardim_area = comprimento * largura;
 
 
 printf("\n--- Resultado do cálculo do jardim  ---\n");
 printf("O perímetro do jardim é: %f\n", perimetro);
-printf("A área do jardim é: %f\n", jarim_area);
+printf("A área do jardim é: %f\n", jardim_area);
 
 return 0;
 }
